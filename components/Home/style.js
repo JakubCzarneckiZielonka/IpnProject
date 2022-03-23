b1e0b6c3f4166b3995f774917a3dc0d3f9c985f6
Tartill <taras3052@gmail.com>
2022-03-23 10:07:18 +0100

    Create style.js

---
+++ components/Home/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    view:{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "red", 
+
+    },
+    txt:{
+        color: "#fff", 
+        fontSize: 80
+    }
+})
+
+export default styles;
