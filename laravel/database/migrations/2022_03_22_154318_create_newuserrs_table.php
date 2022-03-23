f6c1107834068d375a54313a287d522bd0407b94
Tartill <taras3052@gmail.com>
2022-03-23 10:09:21 +0100

    laravel

---
+++ laravel/database/migrations/2022_03_22_154318_create_newuserrs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newuserrs', function (Blueprint $table) {
+            $table->integer('UserID')->primary();
+            $table->string('UserEmail', 100);
+            $table->string('UserPw', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('newuserrs');
+    }
+};
