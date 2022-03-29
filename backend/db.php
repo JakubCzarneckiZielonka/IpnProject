a0cbd97a5e2553b7d7eba44148a8c91efca21749
Tartill <101634417+Tartill@users.noreply.github.com>
2022-03-29 10:21:15 +0200

    Delete backend directory

--- backend/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '');
-$database = mysqli_select_db($conn, 'pproject');
-
-$encodedData = file_get_contents('php://input');
-$decodedData = json_decode($encodedData, true);
